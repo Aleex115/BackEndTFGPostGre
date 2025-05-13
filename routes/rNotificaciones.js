837d8566f00cf19ddd442bb59452e8986c86eae9
Alex <alejandropelazasbaena@gmail.com>
2025-05-13 20:17:21 +0200

    put

--- routes/rNotificaciones.js
+++ routes/rNotificaciones.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 let routes = Router();
 import cNotificaciones from "../controllers/cNotificaciones.js";
 
-routes.update("/setRead", cNotificaciones.marcarLeida);
+routes.put("/setRead", cNotificaciones.marcarLeida);
 routes.get("/getAllNotifications", cNotificaciones.getAll);
 
 export default routes;
