f90c0b44a46bbf8137651d074eeb1d5976ba2bf7
Alex <alejandropelazasbaena@gmail.com>
2025-05-16 20:01:39 +0200

    db

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 import { neon } from "@neondatabase/serverless";
 
-const sql = neon(
+const db = neon(
   "postgresql://neondb_owner:npg_7DZdS5VIwvBP@ep-yellow-water-a26l1lwt-pooler.eu-central-1.aws.neon.tech/neondb?sslmode=require"
 );
 
-export default sql;
+export default db;
