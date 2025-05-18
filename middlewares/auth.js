e503b826378f48239481b00f49833f69daed6e36
Alex <alejandropelazasbaena@gmail.com>
2025-05-18 18:23:02 +0200

    cambios

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,6 +3,12 @@ import error from "./error.js";
 export let isAuthenticated = (req, res, next) => {
   if (req.session.user) {
     return next();
+  } else {
+    res.clearCookie("connect.sid", {
+      path: "/",
+      httpOnly: true,
+      secure: false,
+    });
   }
   error.e401(req, res, {
     message: "Unauthorized. Please log in.",
